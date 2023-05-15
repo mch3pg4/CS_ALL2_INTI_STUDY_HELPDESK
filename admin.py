930951c50e8d28d679c121d004b3885684d4ddd4
mch3pg4 <mch3pg4@gmail.com>
2023-05-15 14:19:01 +0800

    admin page receive buttons from buttons file

--- admin.py
+++ admin.py
@@ -1,15 +1,15 @@
 import bcrypt, re, random, io, string
-import datetime
 from tkcalendar import Calendar
 from tkinter import *
 from PIL import Image,ImageTk
 from tkinter import messagebox, ttk, filedialog, Tk, Label, Entry, Button, END
 import tkinter as tk
-from time import strftime
-from datetime import date
 import mysql.connector
 from mysql.connector import Error
-from captcha.image import ImageCaptcha
+from buttons import clockdate, ui_bg, log_out_btn
+from ALL2_test1 import Loginpage
+
+
 
 f=('Arial', 14)
 f2=('Arial', 12)
@@ -24,21 +24,27 @@ class AdminPage(tk.Frame):
         self.parent=parent
 
         #admin page background
-        self.raw_image=Image.open("images\Slide1.png")
-        self.background_image=ImageTk.PhotoImage(self.raw_image)
-        self.background_label = tk.Label(self, image=self.background_image)
-        self.background_label.place(x=-215,y=-155)
-        self.background_label.image = self.background_image
+        ui_bg(self, 'images\\Slide5.png')
 
         #admin page title
         self.admin_title=tk.Label(self, text='Admin Page', font=('Helvetica', 18, 'bold'), bg=bgc)
         self.admin_title.place(x=450, y=10)
 
-        #inti logo
-        # inti_logo(self)
+        #show list of students from db
+
+        #add/delete/view books
+
+        #add/edit/delete quiz
+
+        #chat discussions
+
+        #add/delete/view course materials
+
+        #view appointments
+
 
         #show admin date and clock
-        # adminclock(self)
+        clockdate(self)
 
         #admin view as normal user
         # def view_user():
@@ -47,7 +53,8 @@ class AdminPage(tk.Frame):
         # self.viewuser_btn.place(x=945, y=135)
 
         # #Logout
-        # def log_out():
+        log_out_btn(self, Loginpage, controller)
+        
         #     controller.show_frame(Loginpage)
         #     messagebox.showinfo('Logout Status', 'Logged out successfully!')
         # self.logout_btn=tk.Button(self, height=1, width=9, font=f, command=log_out, text='Logout')
