2d330e507b676c76785e693b560b65ee19671bb8
mch3pg4 <mch3pg4@gmail.com>
2023-05-15 11:21:16 +0800

    Update admin.py

--- admin.py
+++ admin.py
@@ -11,16 +11,27 @@ import mysql.connector
 from mysql.connector import Error
 from captcha.image import ImageCaptcha
 
+f=('Arial', 14)
+f2=('Arial', 12)
+bgc='#F9D3B9'
+
 class AdminPage(tk.Frame):
     def __init__(self,parent=None, controller=None, name=None):
         global login_details
-        tk.Frame.__init__(self,parent,bg='AntiqueWhite1')
+        tk.Frame.__init__(self,parent)
         self.controller=controller
         self.name=name
         self.parent=parent
 
+        #admin page background
+        self.raw_image=Image.open("images\Slide1.png")
+        self.background_image=ImageTk.PhotoImage(self.raw_image)
+        self.background_label = tk.Label(self, image=self.background_image)
+        self.background_label.place(x=-215,y=-155)
+        self.background_label.image = self.background_image
+
         #admin page title
-        self.admin_title=tk.Label(self, text='Admin Page', font=('Helvetica', 18, 'bold'), bg='AntiqueWhite1')
+        self.admin_title=tk.Label(self, text='Admin Page', font=('Helvetica', 18, 'bold'), bg=bgc)
         self.admin_title.place(x=450, y=10)
 
         #inti logo
